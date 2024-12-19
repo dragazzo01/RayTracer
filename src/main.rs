e5ef12742d21423e907a6f074e397f55a8874b9f
Daniel Ragazzo <dragazzo01@outlook.com>
2024-12-18 21:16:45 -0600

    test commit

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use std::io::{Error, Write};
 fn main() -> Result<(), Error> {
     // Image
     let aspect_ratio = 16.0 / 9.0;
-    let image_width = 400;
+    let image_width = 200;
 
     // Calculate the image height, and ensure that it's at least 1.
     let image_height = (image_width as f64 / aspect_ratio) as i32;
